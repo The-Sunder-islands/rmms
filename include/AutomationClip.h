The-Sunder-islands/rmms

commit 1b3ad487df66d8d361b4b0cf0e7db96c7196bb68

Rename TCO and related to Clip (#6226)
This PR renames

    TCO -> Clip
    Pattern -> MidiClip
    *TCO and *TCOView -> *Clip and *ClipView

The savefiles are not yet modified by this PR.

include/AutomationClip.h
@@ -1,6 +1,6 @@
 /*
- * AutomationPattern.h - declaration of class AutomationPattern, which contains
- *                       all information about an automation pattern
+ * AutomationClip.h - declaration of class AutomationClip, which contains
+ *                       all information about an automation clip
  *
  * Copyright (c) 2008-2014 Tobias Doerffel <tobydox/at/users.sourceforge.net>
  * Copyright (c) 2006-2008 Javier Serrano Polo <jasp00/at/users.sourceforge.net>
@@ -24,14 +24,14 @@
  *
  */
 
-#ifndef AUTOMATION_PATTERN_H
-#define AUTOMATION_PATTERN_H
+#ifndef AUTOMATION_CLIP_H
+#define AUTOMATION_CLIP_H
 
 #include <QtCore/QMap>
 #include <QtCore/QPointer>
 
 #include "AutomationNode.h"
-#include "TrackContentObject.h"
+#include "Clip.h"
 
 
 class AutomationTrack;
@@ -39,7 +39,7 @@ class TimePos;
 
 
 
-class LMMS_EXPORT AutomationPattern : public TrackContentObject
+class LMMS_EXPORT AutomationClip : public Clip
 {
 	Q_OBJECT
 public:
@@ -55,9 +55,9 @@ public:
 
 	using TimemapIterator = timeMap::const_iterator;
 
-	AutomationPattern( AutomationTrack * _auto_track );
-	AutomationPattern( const AutomationPattern & _pat_to_copy );
-	virtual ~AutomationPattern() = default;
+	AutomationClip( AutomationTrack * _auto_track );
+	AutomationClip( const AutomationClip & _clip_to_copy );
+	virtual ~AutomationClip() = default;
 
 	bool addObject( AutomatableModel * _obj, bool _search_dup = true );
 
@@ -149,15 +149,15 @@ public:
 	void saveSettings( QDomDocument & _doc, QDomElement & _parent ) override;
 	void loadSettings( const QDomElement & _this ) override;
 
-	static const QString classNodeName() { return "automationpattern"; }
+	static const QString classNodeName() { return "automationclip"; }
 	QString nodeName() const override { return classNodeName(); }
 
-	TrackContentObjectView * createView( TrackView * _tv ) override;
+	ClipView * createView( TrackView * _tv ) override;
 
 
 	static bool isAutomated( const AutomatableModel * _m );
-	static QVector<AutomationPattern *> patternsForModel( const AutomatableModel * _m );
-	static AutomationPattern * globalAutomationPattern( AutomatableModel * _m );
+	static QVector<AutomationClip *> clipsForModel( const AutomatableModel * _m );
+	static AutomationClip * globalAutomationClip( AutomatableModel * _m );
 	static void resolveAllIDs();
 
 	bool isRecording() const { return m_isRecording; }
@@ -179,9 +179,9 @@ private:
 	void generateTangents(timeMap::iterator it, int numToGenerate);
 	float valueAt( timeMap::const_iterator v, int offset ) const;
 
-	// Mutex to make methods involving automation patterns thread safe
+	// Mutex to make methods involving automation clips thread safe
 	// Mutable so we can lock it from const objects
-	mutable QMutex m_patternMutex;
+	mutable QMutex m_clipMutex;
 
 	AutomationTrack * m_autoTrack;
 	QVector<jo_id_t> m_idsToResolve;
@@ -204,40 +204,40 @@ private:
 	static const float DEFAULT_MIN_VALUE;
 	static const float DEFAULT_MAX_VALUE;
 
-	friend class AutomationPatternView;
+	friend class AutomationClipView;
 	friend class AutomationNode;
 
 } ;
 
-//Short-hand functions to access node values in an automation pattern;
+//Short-hand functions to access node values in an automation clip;
 // replacement for CPP macros with the same purpose; could be refactored
 // further in the future.
-inline float INVAL(AutomationPattern::TimemapIterator it)
+inline float INVAL(AutomationClip::TimemapIterator it)
 {
 	return it->getInValue();
 }
 
-inline float OUTVAL(AutomationPattern::TimemapIterator it)
+inline float OUTVAL(AutomationClip::TimemapIterator it)
 {
 	return it->getOutValue();
 }
 
-inline float OFFSET(AutomationPattern::TimemapIterator it)
+inline float OFFSET(AutomationClip::TimemapIterator it)
 {
 	return it->getValueOffset();
 }
 
-inline float INTAN(AutomationPattern::TimemapIterator it)
+inline float INTAN(AutomationClip::TimemapIterator it)
 {
 	return it->getInTangent();
 }
 
-inline float OUTTAN(AutomationPattern::TimemapIterator it)
+inline float OUTTAN(AutomationClip::TimemapIterator it)
 {
 	return it->getOutTangent();
 }
 
-inline int POS(AutomationPattern::TimemapIterator it)
+inline int POS(AutomationClip::TimemapIterator it)
 {
 	return it.key();
 }

include/BBClip.h
@@ -1,5 +1,5 @@
 /*
- * BBTCO.h
+ * BBClip.h
  *
  * Copyright (c) 2004-2014 Tobias Doerffel <tobydox/at/users.sourceforge.net>
  *
@@ -23,31 +23,31 @@
  */
  
 
-#ifndef BB_TCO_H
-#define BB_TCO_H
+#ifndef BB_CLIP_H
+#define BB_CLIP_H
 
-#include "TrackContentObjectView.h"
+#include "ClipView.h"
 
 
-class BBTCO : public TrackContentObject
+class BBClip : public Clip
 {
 public:
-	BBTCO( Track * _track );
-	virtual ~BBTCO() = default;
+	BBClip( Track * _track );
+	virtual ~BBClip() = default;
 
 	void saveSettings( QDomDocument & _doc, QDomElement & _parent ) override;
 	void loadSettings( const QDomElement & _this ) override;
 	inline QString nodeName() const override
 	{
-		return( "bbtco" );
+		return( "bbclip" );
 	}
 
 	int bbTrackIndex();
 
-	TrackContentObjectView * createView( TrackView * _tv ) override;
+	ClipView * createView( TrackView * _tv ) override;
 
 private:
-	friend class BBTCOView;
+	friend class BBClipView;
 } ;
 
 

include/SampleClip.h
@@ -1,5 +1,5 @@
 /*
- * SampleTCO.h
+ * SampleClip.h
  *
  * Copyright (c) 2005-2014 Tobias Doerffel <tobydox/at/users.sourceforge.net>
  *
@@ -22,24 +22,24 @@
  *
  */
  
-#ifndef SAMPLE_TCO_H
-#define SAMPLE_TCO_H
+#ifndef SAMPLE_CLIP_H
+#define SAMPLE_CLIP_H
 
 #include "SampleBuffer.h"
 #include "SampleTrack.h"
-#include "TrackContentObject.h"
+#include "Clip.h"
  
 
-class SampleTCO : public TrackContentObject
+class SampleClip : public Clip
 {
 	Q_OBJECT
 	mapPropertyFromModel(bool,isRecord,setRecord,m_recordModel);
 public:
-	SampleTCO( Track * _track );
-	SampleTCO( const SampleTCO& orig );
-	virtual ~SampleTCO();
+	SampleClip( Track * _track );
+	SampleClip( const SampleClip& orig );
+	virtual ~SampleClip();
 
-	SampleTCO& operator=( const SampleTCO& that ) = delete;
+	SampleClip& operator=( const SampleClip& that ) = delete;
 
 	void changeLength( const TimePos & _length ) override;
 	const QString & sampleFile() const;
@@ -48,7 +48,7 @@ public:
 	void loadSettings( const QDomElement & _this ) override;
 	inline QString nodeName() const override
 	{
-		return "sampletco";
+		return "sampleclip";
 	}
 
 	SampleBuffer* sampleBuffer()
@@ -59,7 +59,7 @@ public:
 	TimePos sampleLength() const;
 	void setSampleStartFrame( f_cnt_t startFrame );
 	void setSamplePlayLength( f_cnt_t length );
-	TrackContentObjectView * createView( TrackView * _tv ) override;
+	ClipView * createView( TrackView * _tv ) override;
 
 
 	bool isPlaying() const;
@@ -71,7 +71,7 @@ public slots:
 	void updateLength();
 	void toggleRecord();
 	void playbackPositionChanged();
-	void updateTrackTcos();
+	void updateTrackClips();
 
 
 private:
@@ -79,7 +79,7 @@ private:
 	BoolModel m_recordModel;
 	bool m_isPlaying;
 
-	friend class SampleTCOView;
+	friend class SampleClipView;
 
 
 signals:

src/core/DataFile.cpp
@@ -54,7 +54,7 @@ static void findIds(const QDomElement& elem, QList<jo_id_t>& idList);
 
 // QMap with the DOM elements that access file resources
 const DataFile::ResourcesMap DataFile::ELEMENTS_WITH_RESOURCES = {
-{ "sampletco", {"src"} },
+{ "sampleclip", {"src"} },
 { "audiofileprocessor", {"src"} },
 };
 
@@ -95,7 +95,7 @@ DataFile::typeDescStruct
 	{ DataFile::ClipboardData, "clipboard-data" },
 	{ DataFile::JournalData, "journaldata" },
 	{ DataFile::EffectSettings, "effectsettings" },
-	{ DataFile::NotePattern, "pattern" }
+	{ DataFile::MidiClip, "pattern" }
 } ;
 
 
@@ -200,7 +200,7 @@ bool DataFile::validate( QString extension )
 			return true;
 		}
 		break;
-	case Type::NotePattern:
+	case Type::MidiClip:
 		if (extension == "xpt" || extension == "xptz")
 		{
 			return true;
@@ -915,7 +915,7 @@ void DataFile::upgrade_0_4_0_20080409()
 {
 	// Upgrade to version 0.4.0-20080409 from some version greater than or equal to 0.4.0-20080129
 	QStringList s;
-	s << "note" << "pattern" << "bbtco" << "sampletco" << "time";
+	s << "note" << "pattern" << "bbclip" << "sampleclip" << "time";
 	for( QStringList::iterator it = s.begin(); it < s.end(); ++it )
 	{
 		QDomNodeList list = elementsByTagName( *it );
@@ -1625,7 +1625,7 @@ void DataFile::upgrade_noHiddenClipNames()
 
 		QDomNodeList instClips = track.elementsByTagName("pattern");
 		QDomNodeList autoClips = track.elementsByTagName("automationpattern");
-		QDomNodeList bbClips = track.elementsByTagName("bbtco");
+		QDomNodeList bbClips = track.elementsByTagName("bbclip");
 
 		clearDefaultNames(instClips, trackName);
 		clearDefaultNames(autoClips, trackName);

commit 942adc1f374aae112f99c076f0b84011ae56f074

Fix broken data file upgrade (#6279)
Fixes issues introduced in previous commit (1b3ad487df66d8d361b4b0cf0e7db96c7196bb68),
which affected loading, saving and drag-dropping of clips.

include/AutomationClip.h
@@ -149,7 +149,7 @@ public:
 	void saveSettings( QDomDocument & _doc, QDomElement & _parent ) override;
 	void loadSettings( const QDomElement & _this ) override;
 
-	static const QString classNodeName() { return "automationclip"; }
+	static const QString classNodeName() { return "automationpattern"; }
 	QString nodeName() const override { return classNodeName(); }
 
 	ClipView * createView( TrackView * _tv ) override;

include/BBClip.h
@@ -39,7 +39,7 @@ public:
 	void loadSettings( const QDomElement & _this ) override;
 	inline QString nodeName() const override
 	{
-		return( "bbclip" );
+		return( "bbtco" );
 	}
 
 	int bbTrackIndex();

include/SampleClip.h
@@ -48,7 +48,7 @@ public:
 	void loadSettings( const QDomElement & _this ) override;
 	inline QString nodeName() const override
 	{
-		return "sampleclip";
+		return "sampletco";
 	}
 
 	SampleBuffer* sampleBuffer()

src/core/DataFile.cpp
@@ -54,7 +54,7 @@ static void findIds(const QDomElement& elem, QList<jo_id_t>& idList);
 
 // QMap with the DOM elements that access file resources
 const DataFile::ResourcesMap DataFile::ELEMENTS_WITH_RESOURCES = {
-{ "sampleclip", {"src"} },
+{ "sampletco", {"src"} },
 { "audiofileprocessor", {"src"} },
 };
 
@@ -915,7 +915,7 @@ void DataFile::upgrade_0_4_0_20080409()
 {
 	// Upgrade to version 0.4.0-20080409 from some version greater than or equal to 0.4.0-20080129
 	QStringList s;
-	s << "note" << "pattern" << "bbclip" << "sampleclip" << "time";
+	s << "note" << "pattern" << "bbtco" << "sampletco" << "time";
 	for( QStringList::iterator it = s.begin(); it < s.end(); ++it )
 	{
 		QDomNodeList list = elementsByTagName( *it );
@@ -1625,7 +1625,7 @@ void DataFile::upgrade_noHiddenClipNames()
 
 		QDomNodeList instClips = track.elementsByTagName("pattern");
 		QDomNodeList autoClips = track.elementsByTagName("automationpattern");
-		QDomNodeList bbClips = track.elementsByTagName("bbclip");
+		QDomNodeList bbClips = track.elementsByTagName("bbtco");
 
 		clearDefaultNames(instClips, trackName);
 		clearDefaultNames(autoClips, trackName);