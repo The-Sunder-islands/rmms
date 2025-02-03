The-Sunder-islands/rmms

commit dfa51bae4fc141dbe105015f41abd9b04da1189e

CPack: Refactor AppImage and Apple DMG Generation (#7252)
* CPack: Refactor AppImage and Apple DMG Generation
* Switch from linuxdeployqt to linuxdelpoy
* Add ARM64 AppImage support
* Add support for `.run` installers using `makeself`, an alternative to AppImage
* Refactor BashCompletion.cmake
* Enable CPack debugging via `WANT_DEBUG_CPACK`
* Add `download_binary`, `create_symlink` macros
* Qt6: Fix @rpath bug on macOS
* Detect and bundle LV2 UI Suil modules (Related #7201)
* Allow remote plugins to honor `LMMS_PLUGIN_DIR`
* Add .github/workflows/deps-ubuntu-24.04-gcc.txt
* Fix waveforms FileDialog

Co-authored-by: Dalton Messmer <messmer.dalton@gmail.com>

cmake/apple/MacDeployQt.cmake
@@ -1,0 +1,172 @@
+# Create a macOS .dmg desktop installer using macdeployqt
+#
+# Copyright (c) 2025, Tres Finocchiaro, <tres.finocchiaro@gmail.com>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
+# Variables must be prefixed with "CPACK_" to be visible here
+set(lmms "${CPACK_PROJECT_NAME}")
+set(APP "${CPACK_TEMPORARY_INSTALL_DIRECTORY}/${CPACK_PROJECT_NAME_UCASE}.app")
+
+# Toggle command echoing & verbosity
+# 0 = no output, 1 = error/warning, 2 = normal, 3 = debug
+if(NOT CPACK_DEBUG)
+	set(VERBOSITY 1)
+	set(COMMAND_ECHO NONE)
+else()
+	set(VERBOSITY 2)
+	set(COMMAND_ECHO STDOUT)
+endif()
+
+# Detect release|debug build
+if(NOT CPACK_STRIP_FILES_ORIG)
+	# -use-debug-libs implies -no-strip
+	if(CPACK_QMAKE_EXECUTABLE MATCHES "/homebrew/|/usr/local")
+		message(STATUS "Homebrew does not provide debug qt libraries, replacing \"-use-debug-libs\" with \"-no-strip\" instead")
+		set(USE_DEBUG_LIBS -no-strip)
+	else()
+		set(USE_DEBUG_LIBS -use-debug-libs)
+	endif()
+endif()
+
+# Cleanup CPack "External" json, txt files, old DMG files
+file(GLOB cleanup "${CPACK_BINARY_DIR}/${lmms}-*.json"
+	"${CPACK_BINARY_DIR}/${lmms}-*.dmg"
+	"${CPACK_BINARY_DIR}/install_manifest.txt")
+list(SORT cleanup)
+file(REMOVE ${cleanup})
+
+# Create bundle structure
+file(MAKE_DIRECTORY "${APP}/Contents/MacOS")
+file(MAKE_DIRECTORY "${APP}/Contents/Frameworks")
+file(MAKE_DIRECTORY "${APP}/Contents/Resources")
+
+# Fix layout
+file(RENAME "${CPACK_TEMPORARY_INSTALL_DIRECTORY}/lib" "${APP}/Contents/lib")
+file(RENAME "${CPACK_TEMPORARY_INSTALL_DIRECTORY}/share" "${APP}/Contents/share")
+file(RENAME "${CPACK_TEMPORARY_INSTALL_DIRECTORY}/bin" "${APP}/Contents/bin")
+
+# Move binaries into Contents/MacOS
+file(RENAME "${APP}/Contents/bin/${lmms}" "${APP}/Contents/MacOS/${lmms}")
+file(RENAME "${APP}/Contents/lib/${lmms}/RemoteZynAddSubFx" "${APP}/Contents/MacOS/RemoteZynAddSubFx")
+file(REMOVE_RECURSE "${APP}/Contents/bin")
+file(REMOVE_RECURSE "${APP}/Contents/share/man1")
+file(REMOVE_RECURSE "${APP}/Contents/include")
+
+# Copy missing files
+# Convert https://lmms.io to io.lmms
+string(REPLACE "." ";" mime_parts "${CPACK_PROJECT_URL}")
+string(REPLACE ":" ";" mime_parts "${mime_parts}")
+string(REPLACE "/" "" mime_parts "${mime_parts}")
+list(REMOVE_AT mime_parts 0)
+list(REVERSE mime_parts)
+list(JOIN mime_parts "." MACOS_MIMETYPE_ID)
+configure_file("${CPACK_CURRENT_SOURCE_DIR}/lmms.plist.in" "${APP}/Contents/Info.plist" @ONLY)
+file(COPY "${CPACK_CURRENT_SOURCE_DIR}/project.icns" DESTINATION "${APP}/Contents/Resources")
+file(COPY "${CPACK_CURRENT_SOURCE_DIR}/icon.icns" DESTINATION "${APP}/Contents/Resources")
+file(RENAME "${APP}/Contents/Resources/icon.icns" "${APP}/Contents/Resources/${lmms}.icns")
+
+# Copy Suil modules
+if(CPACK_SUIL_MODULES)
+	set(SUIL_MODULES_TARGET "${APP}/Contents/Frameworks/${CPACK_SUIL_MODULES_PREFIX}")
+	file(MAKE_DIRECTORY "${SUIL_MODULES_TARGET}")
+	file(COPY ${CPACK_SUIL_MODULES} DESTINATION "${SUIL_MODULES_TARGET}")
+endif()
+
+# Make all libraries writable for macdeployqt
+file(CHMOD_RECURSE "${APP}/Contents" PERMISSIONS
+	OWNER_EXECUTE OWNER_WRITE OWNER_READ
+	GROUP_EXECUTE GROUP_WRITE GROUP_READ
+	WORLD_READ)
+
+# Qt6: Fix @rpath bug https://github.com/orgs/Homebrew/discussions/2823
+include(CreateSymlink)
+get_filename_component(QTBIN "${CPACK_QMAKE_EXECUTABLE}" DIRECTORY)
+get_filename_component(QTDIR "${QTBIN}" DIRECTORY)
+create_symlink("${QTDIR}/lib" "${CPACK_TEMPORARY_INSTALL_DIRECTORY}/lib")
+
+# Replace @rpath with @loader_path for Carla
+execute_process(COMMAND install_name_tool -change
+	"@rpath/libcarlabase.dylib"
+	"@loader_path/libcarlabase.dylib"
+	"${APP}/Contents/lib/${lmms}/libcarlapatchbay.so"
+	COMMAND_ECHO ${COMMAND_ECHO}
+	COMMAND_ERROR_IS_FATAL ANY)
+execute_process(COMMAND install_name_tool -change
+	"@rpath/libcarlabase.dylib"
+	"@loader_path/libcarlabase.dylib"
+	"${APP}/Contents/lib/${lmms}/libcarlarack.so"
+	COMMAND_ECHO ${COMMAND_ECHO}
+	COMMAND_ERROR_IS_FATAL ANY)
+
+# Build list of executables to inform macdeployqt about
+# e.g. -executable=foo.dylib -executable=bar.dylib
+file(GLOB LIBS "${APP}/Contents/lib/${lmms}/*.so")
+
+# Inform macdeployqt about LADSPA plugins; may depend on bundled fftw3f, etc.
+file(GLOB LADSPA "${APP}/Contents/lib/${lmms}/ladspa/*.so")
+
+# Inform linuxdeploy about remote plugins
+list(APPEND REMOTE_PLUGINS "${APP}/Contents/MacOS/*Remote*")
+
+# Collect, sort and dedupe all libraries
+list(APPEND LIBS ${LADSPA})
+list(APPEND LIBS ${REMOTE_PLUGINS})
+list(APPEND LIBS ${CPACK_SUIL_MODULES})
+list(REMOVE_DUPLICATES LIBS)
+list(SORT LIBS)
+
+# Construct macdeployqt parameters
+foreach(_lib IN LISTS LIBS)
+	if(EXISTS "${_lib}")
+		list(APPEND EXECUTABLES "-executable=${_lib}")
+	endif()
+endforeach()
+
+# Call macdeployqt
+get_filename_component(QTBIN "${CPACK_QMAKE_EXECUTABLE}" DIRECTORY)
+message(STATUS "Calling ${QTBIN}/macdeployqt ${APP} [... executables]")
+execute_process(COMMAND "${QTBIN}/macdeployqt" "${APP}" ${EXECUTABLES}
+	-verbose=${VERBOSITY}
+	${USE_DEBUG_LIBS}
+	COMMAND_ECHO ${COMMAND_ECHO}
+	COMMAND_ERROR_IS_FATAL ANY)
+
+# Cleanup symlink
+file(REMOVE "${CPACK_TEMPORARY_INSTALL_DIRECTORY}/lib")
+
+# Remove dummy carla libs, relink to a sane location (e.g. /Applications/Carla.app/...)
+# (must be done after calling macdeployqt)
+file(GLOB CARLALIBS "${APP}/Contents/lib/${lmms}/libcarla*")
+foreach(_carlalib IN LISTS CARLALIBS)
+	foreach(_lib "${CPACK_CARLA_LIBRARIES}")
+		set(_oldpath "../../Frameworks/lib${_lib}.dylib")
+		set(_newpath "Carla.app/Contents/MacOS/lib${_lib}.dylib")
+		execute_process(COMMAND install_name_tool -change
+			"@loader_path/${_oldpath}"
+			"@executable_path/../../../${_newpath}"
+			"${_carlalib}"
+			COMMAND_ECHO ${COMMAND_ECHO}
+			COMMAND_ERROR_IS_FATAL ANY)
+		file(REMOVE "${APP}/Contents/Frameworks/lib${_lib}.dylib")
+	endforeach()
+endforeach()
+
+# Call ad-hoc codesign manually (CMake offers this as well)
+execute_process(COMMAND codesign --force --deep --sign - "${APP}"
+	COMMAND_ECHO ${COMMAND_ECHO}
+	COMMAND_ERROR_IS_FATAL ANY)
+
+# Create DMG
+# appdmg won't allow volume names > 27 char https://github.com/LinusU/node-alias/issues/7
+find_program(APPDMG_BIN appdmg REQUIRED)
+string(SUBSTRING "${CPACK_PROJECT_NAME_UCASE} ${CPACK_PROJECT_VERSION}" 0 27 APPDMG_VOLUME_NAME)
+# We'll configure this file twice (again in MacDeployQt.cmake once we know CPACK_TEMPORARY_INSTALL_DIRECTORY)
+configure_file("${CPACK_CURRENT_SOURCE_DIR}/appdmg.json.in" "${CPACK_CURRENT_BINARY_DIR}/appdmg.json" @ONLY)
+
+execute_process(COMMAND "${APPDMG_BIN}"
+	"${CPACK_CURRENT_BINARY_DIR}/appdmg.json"
+	"${CPACK_BINARY_DIR}/${CPACK_PACKAGE_FILE_NAME}.dmg"
+	COMMAND_ECHO ${COMMAND_ECHO}
+    COMMAND_ERROR_IS_FATAL ANY)

src/core/RemotePlugin.cpp
@@ -235,6 +235,11 @@ bool RemotePlugin::init(const QString &pluginExecutable,
 		m_failed = false;
 	}
 	QString exec = QFileInfo(QDir("plugins:"), pluginExecutable).absoluteFilePath();
+
+	// We may have received a directory via a environment variable
+	if (const char* env_path = std::getenv("LMMS_PLUGIN_DIR"))
+			exec = QFileInfo(QDir(env_path), pluginExecutable).absoluteFilePath();
+
 #ifdef LMMS_BUILD_APPLE
 	// search current directory first
 	QString curDir = QCoreApplication::applicationDirPath() + "/" + pluginExecutable;
@@ -252,7 +257,7 @@ bool RemotePlugin::init(const QString &pluginExecutable,
 
 	if( ! QFile( exec ).exists() )
 	{
-		qWarning( "Remote plugin '%s' not found.",
+		qWarning( "Remote plugin '%s' not found",
 						exec.toUtf8().constData() );
 		m_failed = true;
 		invalidate();

.github/workflows/build.yml
@@ -5,8 +5,8 @@ concurrency:
   group: ${{ github.workflow }}-${{ github.ref }}
   cancel-in-progress: true
 jobs:
-  linux:
-    name: linux
+  linux-x86_64:
+    name: linux-x86_64
     runs-on: ubuntu-latest
     container: ghcr.io/lmms/linux.gcc:20.04
     env:
@@ -39,7 +39,6 @@ jobs:
           source /opt/qt5*/bin/qt5*-env.sh || true
           cmake -S . \
                 -B build \
-                -DCMAKE_INSTALL_PREFIX=./install \
                 $CMAKE_OPTS
       - name: Build
         run: cmake --build build
@@ -49,12 +48,76 @@ jobs:
           ctest --output-on-failure -j2
       - name: Package
         run: |
-          cmake --build build --target install
-          cmake --build build --target appimage
+          cmake --build build --target package
       - name: Upload artifacts
         uses: actions/upload-artifact@v4
         with:
           name: linux
+          path: build/lmms-*.AppImage
+      - name: Trim ccache and print statistics
+        run: |
+          ccache --cleanup
+          echo "[ccache config]"
+          ccache --show-config
+          echo "[ccache stats]"
+          ccache --show-stats
+        env:
+          CCACHE_MAXSIZE: 500M
+  linux-arm64:
+    name: linux-arm64
+    runs-on: ubuntu-24.04-arm
+    env:
+      CMAKE_OPTS: >-
+        -DUSE_WERROR=ON
+        -DCMAKE_BUILD_TYPE=RelWithDebInfo
+        -DUSE_COMPILE_CACHE=ON
+        -DWANT_DEBUG_CPACK=ON
+      CCACHE_MAXSIZE: 0
+      CCACHE_NOCOMPRESS: 1
+      MAKEFLAGS: -j2
+      DEBIAN_FRONTEND: noninteractive
+    steps:
+      - name: Configure git
+        run: git config --global --add safe.directory "$GITHUB_WORKSPACE"
+      - name: Check out
+        uses: actions/checkout@v3
+        with:
+          fetch-depth: 0
+          submodules: recursive
+      - name: Install system packages
+        run: |
+          sudo apt-get update -y
+          sudo apt-get install -y --no-install-recommends \
+            $(xargs < .github/workflows/deps-ubuntu-24.04-gcc.txt)
+      - name: Cache ccache data
+        uses: actions/cache@v3
+        with:
+          key: ccache-${{ github.job }}-${{ github.ref }}-${{ github.run_id }}
+          restore-keys: |
+            ccache-${{ github.job }}-${{ github.ref }}-
+            ccache-${{ github.job }}-
+          path: ~/.ccache
+      - name: Configure
+        run: |
+          ccache --zero-stats
+          cmake -S . \
+                -B build \
+                -DWANT_VST_32=OFF \
+                -DWANT_VST_64=OFF \
+                $CMAKE_OPTS
+      - name: Build
+        run: cmake --build build
+      - name: Run tests
+        run: |
+          cd build/tests
+          ctest --output-on-failure -j2
+      - name: Package
+        run: |
+          cmake --build build --target package
+      - name: Upload artifacts
+        uses: actions/upload-artifact@v4
+        with:
+          name: linux-arm64
           path: build/lmms-*.AppImage
       - name: Trim ccache and print statistics
         run: |
@@ -129,7 +192,6 @@ jobs:
           mkdir build
           cmake -S . \
                 -B build \
-                -DCMAKE_INSTALL_PREFIX="../target" \
                 -DCMAKE_PREFIX_PATH="$(brew --prefix qt@5)" \
                 -DCMAKE_OSX_ARCHITECTURES=${{ matrix.arch }} \
                 $CMAKE_OPTS \
@@ -142,8 +204,7 @@ jobs:
           ctest --output-on-failure -j3
       - name: Package
         run: |
-          cmake --build build --target install
-          cmake --build build --target dmg
+          cmake --build build --target package
       - name: Upload artifacts
         uses: actions/upload-artifact@v4
         with:
@@ -208,7 +269,6 @@ jobs:
           ccache --zero-stats
           cmake -S . \
                 -B build \
-                -DCMAKE_INSTALL_PREFIX=./install \
                 -DCMAKE_TOOLCHAIN_FILE="./cmake/toolchains/MinGW-W64-64.cmake" \
                 $CMAKE_OPTS
       - name: Build

CMakeLists.txt
@@ -104,6 +104,8 @@ option(WANT_DEBUG_MSAN	"Enable MemorySanitizer" OFF)
 option(WANT_DEBUG_UBSAN	"Enable UndefinedBehaviorSanitizer" OFF)
 option(WANT_DEBUG_GPROF	"Enable gprof profiler" OFF)
 OPTION(BUNDLE_QT_TRANSLATIONS	"Install Qt translation files for LMMS" OFF)
+option(WANT_DEBUG_CPACK "Show detailed logs for packaging commands" OFF)
+option(WANT_CPACK_TARBALL "Request CPack to create a tarball instead of an installer" OFF)
 
 
 IF(LMMS_BUILD_APPLE)
@@ -268,6 +270,7 @@ IF(WANT_SUIL)
 		IF(SUIL_FOUND)
 			SET(LMMS_HAVE_SUIL TRUE)
 			SET(STATUS_SUIL "OK")
+			find_package(SuilModules)
 		ELSE()
 			SET(STATUS_SUIL "not found, install it or set PKG_CONFIG_PATH appropriately")
 		ENDIF()
@@ -606,6 +609,17 @@ ELSE()
 	SET (STATUS_DEBUG_FPE "Disabled")
 ENDIF(WANT_DEBUG_FPE)
 
+if(WANT_DEBUG_CPACK)
+	if((LMMS_BUILD_WIN32 AND CMAKE_VERSION VERSION_LESS "3.19") OR WANT_CPACK_TARBALL)
+		set(STATUS_DEBUG_CPACK "Wanted but disabled due to unsupported configuration")
+	else()
+		set(CPACK_DEBUG TRUE)
+		set(STATUS_DEBUG_CPACK "Enabled")
+	endif()
+else()
+	set(STATUS_DEBUG_CPACK "Disabled")
+endif()
+
 # check for libsamplerate
 FIND_PACKAGE(Samplerate 0.1.8 MODULE REQUIRED)
 
@@ -679,7 +693,7 @@ if(WANT_DEBUG_GPROF)
 	add_link_options(-pg)
 	set(STATUS_GPROF "OK")
 else()
-	set(STATUS_GPROF "DISABLED ${STATUS_GPROF}")
+	set(STATUS_GPROF "Disabled ${STATUS_GPROF}")
 endif()
 
 # add enabled sanitizers
@@ -745,7 +759,7 @@ IF(LMMS_BUILD_LINUX)
 		"${CMAKE_BINARY_DIR}/lmmsconfig.h"
 		"${CMAKE_BINARY_DIR}/lmmsversion.h"
 		"${CMAKE_SOURCE_DIR}/src/gui/embed.cpp"
-		DESTINATION "${CMAKE_INSTALL_PREFIX}/include/lmms/")
+		DESTINATION "include/lmms/")
 ENDIF(LMMS_BUILD_LINUX)
 
 #
@@ -849,6 +863,7 @@ MESSAGE(
 "* Debug using ThreadSanitizer       : ${STATUS_DEBUG_TSAN}\n"
 "* Debug using MemorySanitizer       : ${STATUS_DEBUG_MSAN}\n"
 "* Debug using UBSanitizer           : ${STATUS_DEBUG_UBSAN}\n"
+"* Debug packaging commands          : ${STATUS_DEBUG_CPACK}\n"
 "* Profile using GNU profiler        : ${STATUS_GPROF}\n"
 )
 

cmake/CMakeLists.txt
@@ -14,10 +14,14 @@ ENDIF()
 SET(CPACK_PACKAGE_INSTALL_DIRECTORY             "${PROJECT_NAME_UCASE}")
 SET(CPACK_SOURCE_GENERATOR                      "TBZ2")
 SET(CPACK_SOURCE_PACKAGE_FILE_NAME              "${CMAKE_PROJECT_NAME}-${VERSION}")
-IF(NOT DEFINED WIN32)
-	SET(CPACK_STRIP_FILES                   "bin/${CMAKE_PROJECT_NAME};${PLUGIN_DIR}/*.so")
-	SET(CPACK_PACKAGE_EXECUTABLES           "${CMAKE_PROJECT_NAME}" "${PROJECT_NAME_UCASE} binary")
-ENDIF()
+SET(CPACK_PACKAGE_EXECUTABLES                   "${CMAKE_PROJECT_NAME}" "${PROJECT_NAME_UCASE} binary")
+
+# Disable strip for Debug|RelWithDebInfo
+if(CMAKE_BUILD_TYPE MATCHES "Deb")
+	unset(CPACK_STRIP_FILES)
+else()
+	set(CPACK_STRIP_FILES TRUE)
+endif()
 
 IF(LMMS_BUILD_WIN32)
 	ADD_SUBDIRECTORY(nsis)

cmake/apple/CMakeLists.txt
@@ -1,24 +1,3 @@
-SET(MACOSX_BUNDLE_ICON_FILE             "icon.icns")
-SET(MACOSX_BUNDLE_GUI_IDENTIFIER        "${PROJECT_NAME_UCASE}")
-SET(MACOSX_BUNDLE_LONG_VERSION_STRING   "${VERSION}")
-SET(MACOSX_BUNDLE_BUNDLE_NAME           "${PROJECT_NAME_UCASE}")
-SET(MACOSX_BUNDLE_SHORT_VERSION_STRING  "${VERSION}")
-SET(MACOSX_BUNDLE_BUNDLE_VERSION        "${VERSION}")
-SET(MACOSX_BUNDLE_COPYRIGHT             "${PROJECT_COPYRIGHT}")
-SET(MACOSX_BUNDLE_MIMETYPE              "application/x-lmms-project")
-SET(MACOSX_BUNDLE_MIMETYPE_ICON         "project.icns")
-SET(MACOSX_BUNDLE_MIMETYPE_ID           "io.lmms")
-SET(MACOSX_BUNDLE_PROJECT_URL           "${PROJECT_URL}")
-SET(MACOSX_BUNDLE_DMG_TITLE             "${MACOSX_BUNDLE_BUNDLE_NAME} ${MACOSX_BUNDLE_LONG_VERSION_STRING}")
-
-# FIXME: appdmg won't allow volume names > 27 char
-# See also https://github.com/LinusU/node-appdmg/issues/48
-STRING(SUBSTRING "${MACOSX_BUNDLE_DMG_TITLE}" 0 27 MACOSX_BUNDLE_DMG_TITLE)
-
-CONFIGURE_FILE("lmms.plist.in"          "${CMAKE_BINARY_DIR}/Info.plist")
-CONFIGURE_FILE("install_apple.sh.in"    "${CMAKE_BINARY_DIR}/install_apple.sh" @ONLY)
-CONFIGURE_FILE("package_apple.json.in"  "${CMAKE_BINARY_DIR}/package_apple.json" @ONLY)
-
 IF(CMAKE_OSX_ARCHITECTURES)
 	SET(DMG_ARCH "${CMAKE_OSX_ARCHITECTURES}")
 ELSEIF(IS_ARM64)
@@ -29,18 +8,31 @@ ELSE()
 	SET(DMG_ARCH "x86_64")
 ENDIF()
 
-# DMG creation target
-SET(DMG_FILE "${CMAKE_BINARY_DIR}/${CMAKE_PROJECT_NAME}-${VERSION}-mac${APPLE_OS_VER}-${DMG_ARCH}.dmg")
+# Standard CPack options
+set(CPACK_GENERATOR "External" PARENT_SCOPE)
+set(CPACK_EXTERNAL_ENABLE_STAGING TRUE PARENT_SCOPE)
+set(CPACK_PACKAGE_FILE_NAME "${CMAKE_PROJECT_NAME}-${VERSION}-mac${APPLE_OS_VER}-${DMG_ARCH}")
+set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_FILE_NAME}" PARENT_SCOPE)
+set(CPACK_PRE_BUILD_SCRIPTS "${CMAKE_CURRENT_SOURCE_DIR}/MacDeployQt.cmake" PARENT_SCOPE)
+# disable cpack's strip: causes missing symbols on macOS
+set(CPACK_STRIP_FILES_ORIG "${CPACK_STRIP_FILES}" PARENT_SCOPE)
+set(CPACK_STRIP_FILES FALSE PARENT_SCOPE)
 
-FILE(REMOVE "${DMG_FILE}")
-ADD_CUSTOM_TARGET(removedmg
-	COMMAND touch "\"${DMG_FILE}\"" && rm "\"${DMG_FILE}\""
-	COMMENT "Removing old DMG")
-ADD_CUSTOM_TARGET(dmg 
-	COMMAND appdmg "\"${CMAKE_BINARY_DIR}/package_apple.json\"" "\"${DMG_FILE}\""
-	DEPENDS "${CMAKE_BINARY_DIR}/package_apple.json"
-	COMMENT "Generating DMG")
-ADD_DEPENDENCIES(dmg removedmg)
+# Custom vars to expose to Cpack
+# must be prefixed with "CPACK_" per https://stackoverflow.com/a/46526757/3196753)
+set(CPACK_CURRENT_BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}" PARENT_SCOPE)
+set(CPACK_CURRENT_SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}" PARENT_SCOPE)
+set(CPACK_BINARY_DIR "${CMAKE_BINARY_DIR}" PARENT_SCOPE)
+set(CPACK_SOURCE_DIR "${CMAKE_SOURCE_DIR}" PARENT_SCOPE)
+set(CPACK_QMAKE_EXECUTABLE "${QT_QMAKE_EXECUTABLE}" PARENT_SCOPE)
+set(CPACK_CARLA_LIBRARIES "${CARLA_LIBRARIES}" PARENT_SCOPE)
+set(CPACK_PROJECT_NAME "${PROJECT_NAME}" PARENT_SCOPE)
+set(CPACK_PROJECT_VERSION "${VERSION}" PARENT_SCOPE)
+set(CPACK_PROJECT_NAME_UCASE "${PROJECT_NAME_UCASE}" PARENT_SCOPE)
+set(CPACK_PROJECT_URL "${PROJECT_URL}" PARENT_SCOPE)
+set(CPACK_SUIL_MODULES "${Suil_MODULES}" PARENT_SCOPE)
+set(CPACK_SUIL_MODULES_PREFIX "${Suil_MODULES_PREFIX}" PARENT_SCOPE)
 
-# see also ../postinstall/CMakeLists.txt
-
+if(CMAKE_VERSION VERSION_LESS "3.19")
+    message(WARNING "DMG creation requires at least CMake 3.19")
+endif()

cmake/apple/appdmg.json.in
@@ -1,0 +1,9 @@
+{
+ "title": "@APPDMG_VOLUME_NAME@",
+ "background": "@CPACK_SOURCE_DIR@/cmake/apple/background.png",
+ "icon-size": 128,
+ "contents": [
+   { "x": 139, "y": 200, "type": "file", "path": "@CPACK_TEMPORARY_INSTALL_DIRECTORY@/@CPACK_PROJECT_NAME_UCASE@.app" },
+   { "x": 568, "y": 200, "type": "link", "path": "/Applications" }
+ ]
+}

commit 87c928968105c156eaf5584b8eb84292a797e6cb

SharedMemory: Make key optional, default to shorter UID on macOS (#7681)
* SharedMemory: Make key optional, shorter on macOS
* Add getters for shared memory size
* macOS: Fix linking regression for RemoteZynAddSubFx introduced with #7252 

---------

Co-authored-by: Dalton Messmer <messmer.dalton@gmail.com>

cmake/apple/MacDeployQt.cmake
@@ -107,8 +107,8 @@ file(GLOB LIBS "${APP}/Contents/lib/${lmms}/*.so")
 # Inform macdeployqt about LADSPA plugins; may depend on bundled fftw3f, etc.
 file(GLOB LADSPA "${APP}/Contents/lib/${lmms}/ladspa/*.so")
 
-# Inform linuxdeploy about remote plugins
-list(APPEND REMOTE_PLUGINS "${APP}/Contents/MacOS/*Remote*")
+# Inform macdeployqt about remote plugins
+file(GLOB REMOTE_PLUGINS "${APP}/Contents/MacOS/*Remote*")
 
 # Collect, sort and dedupe all libraries
 list(APPEND LIBS ${LADSPA})

include/RemotePluginBase.h
@@ -71,7 +71,6 @@
 #include <QProcess>
 #include <QThread>
 #include <QString>
-#include <QUuid>
 
 #ifndef SYNC_WITH_SHM_FIFO
 #include <poll.h>
@@ -125,7 +124,7 @@ public:
 		m_master( true ),
 		m_lockDepth( 0 )
 	{
-		m_data.create(QUuid::createUuid().toString().toStdString());
+		m_data.create();
 		m_data->startPtr = m_data->endPtr = 0;
 		static int k = 0;
 		m_data->dataSem.semKey = ( getpid()<<10 ) + ++k;

include/SharedMemory.h
@@ -44,6 +44,7 @@ public:
 	SharedMemoryData() noexcept;
 	SharedMemoryData(std::string&& key, bool readOnly);
 	SharedMemoryData(std::string&& key, std::size_t size, bool readOnly);
+	SharedMemoryData(std::size_t size, bool readOnly);
 	~SharedMemoryData();
 
 	SharedMemoryData(SharedMemoryData&& other) noexcept;
@@ -64,6 +65,7 @@ public:
 
 	const std::string& key() const noexcept { return m_key; }
 	void* get() const noexcept { return m_ptr; }
+	std::size_t size_bytes() const noexcept;
 
 private:
 	std::string m_key;
@@ -95,6 +97,11 @@ public:
 		m_data = detail::SharedMemoryData{std::move(key), sizeof(T), std::is_const_v<T>};
 	}
 
+	void create()
+	{
+		m_data = detail::SharedMemoryData{sizeof(T), std::is_const_v<T>};
+	}
+
 	void detach() noexcept
 	{
 		m_data = detail::SharedMemoryData{};
@@ -102,6 +109,9 @@ public:
 
 	const std::string& key() const noexcept { return m_data.key(); }
 	T* get() const noexcept { return static_cast<T*>(m_data.get()); }
+
+	std::size_t size() const noexcept { return get() ? 1 : 0; }
+	std::size_t size_bytes() const noexcept { return get() ? sizeof(T) : 0; }
 
 	T* operator->() const noexcept { return get(); }
 	T& operator*() const noexcept { return *get(); }
@@ -132,6 +142,11 @@ public:
 		m_data = detail::SharedMemoryData{std::move(key), size * sizeof(T), std::is_const_v<T>};
 	}
 
+	void create(std::size_t size)
+	{
+		m_data = detail::SharedMemoryData{size * sizeof(T), std::is_const_v<T>};
+	}
+
 	void detach() noexcept
 	{
 		m_data = detail::SharedMemoryData{};
@@ -139,6 +154,9 @@ public:
 
 	const std::string& key() const noexcept { return m_data.key(); }
 	T* get() const noexcept { return static_cast<T*>(m_data.get()); }
+
+	std::size_t size() const noexcept { return m_data.size_bytes() / sizeof(T); }
+	std::size_t size_bytes() const noexcept { return m_data.size_bytes(); }
 
 	T& operator[](std::size_t index) const noexcept { return get()[index]; }
 	explicit operator bool() const noexcept { return get() != nullptr; }

src/common/SharedMemory.cpp
@@ -23,6 +23,7 @@
 
 #include "SharedMemory.h"
 
+#include <random>
 #include <system_error>
 #include <utility>
 
@@ -75,7 +76,7 @@ class SharedMemoryImpl
 {
 public:
 	SharedMemoryImpl(const std::string& key, bool readOnly) :
-		m_key{"/" + key}
+		m_key{'/' + key}
 	{
 		const auto openFlags = readOnly ? O_RDONLY : O_RDWR;
 		const auto fd = FileDescriptor{
@@ -93,7 +94,7 @@ public:
 	}
 
 	SharedMemoryImpl(const std::string& key, std::size_t size, bool readOnly) :
-		m_key{"/" + key},
+		m_key{'/' + key},
 		m_size{size}
 	{
 		const auto fd = FileDescriptor{
@@ -120,11 +121,12 @@ public:
 	}
 
 	auto get() const noexcept -> void* { return m_mapping; }
+	auto size_bytes() const noexcept -> std::size_t { return m_size; }
 
 private:
 	std::string m_key;
-	std::size_t m_size;
-	void* m_mapping;
+	std::size_t m_size = 0;
+	void* m_mapping = nullptr;
 	ShmObject m_object;
 };
 
@@ -163,9 +165,18 @@ public:
 
 		m_view.reset(MapViewOfFile(m_mapping.get(), access, 0, 0, 0));
 		if (!m_view) { throwLastError("SharedMemoryImpl: MapViewOfFile() failed"); }
+
+		MEMORY_BASIC_INFORMATION mbi;
+		if (VirtualQuery(m_view.get(), &mbi, sizeof(mbi)) == 0)
+		{
+			throwLastError("SharedMemoryImpl: VirtualQuery() failed");
+		}
+
+		m_size = static_cast<std::size_t>(mbi.RegionSize);
 	}
 
-	SharedMemoryImpl(const std::string& key, std::size_t size, bool readOnly)
+	SharedMemoryImpl(const std::string& key, std::size_t size, bool readOnly) :
+		m_size{size}
 	{
 		const auto [high, low] = sizeToHighAndLow(size);
 		m_mapping.reset(CreateFileMappingA(INVALID_HANDLE_VALUE, nullptr, PAGE_READWRITE, high, low, key.c_str()));
@@ -186,13 +197,38 @@ public:
 	auto operator=(const SharedMemoryImpl&) -> SharedMemoryImpl& = delete;
 
 	auto get() const noexcept -> void* { return m_view.get(); }
+	auto size_bytes() const noexcept -> std::size_t { return m_size; }
 
 private:
 	UniqueHandle m_mapping;
 	FileView m_view;
+	std::size_t m_size = 0;
 };
 
 #endif
+
+namespace {
+
+auto createKey() -> std::string
+{
+	// Max length (minus prepended '/') on macOS (PSHMNAMLEN=31)
+	constexpr int length = 30;
+
+	std::string key;
+	std::random_device rd;
+	auto gen = std::mt19937{rd()}; // mersenne twister, seeded
+	auto distrib = std::uniform_int_distribution{0, 15}; // hex range (0-15)
+
+	key.reserve(length + 1);
+	for (int i = 0; i < length; ++i)
+	{
+		key += "0123456789ABCDEF"[distrib(gen)];
+	}
+
+	return key;
+}
+
+} // namespace
 
 SharedMemoryData::SharedMemoryData() noexcept = default;
 
@@ -208,6 +244,10 @@ SharedMemoryData::SharedMemoryData(std::string&& key, std::size_t size, bool rea
 	m_ptr{m_impl->get()}
 { }
 
+SharedMemoryData::SharedMemoryData(std::size_t size, bool readOnly) :
+	SharedMemoryData{createKey(), size, readOnly}
+{ }
+
 SharedMemoryData::~SharedMemoryData() = default;
 
 SharedMemoryData::SharedMemoryData(SharedMemoryData&& other) noexcept :
@@ -215,5 +255,10 @@ SharedMemoryData::SharedMemoryData(SharedMemoryData&& other) noexcept :
 	m_impl{std::move(other.m_impl)},
 	m_ptr{std::exchange(other.m_ptr, nullptr)}
 { }
+
+auto SharedMemoryData::size_bytes() const noexcept -> std::size_t
+{
+	return m_impl ? m_impl->size_bytes() : 0;
+}
 
 } // namespace lmms::detail

src/core/RemotePlugin.cpp
@@ -485,7 +485,7 @@ void RemotePlugin::resizeSharedProcessingMemory()
 	const size_t s = (m_inputCount + m_outputCount) * Engine::audioEngine()->framesPerPeriod();
 	try
 	{
-		m_audioBuffer.create(QUuid::createUuid().toString().toStdString(), s);
+		m_audioBuffer.create(s);
 	}
 	catch (const std::runtime_error& error)
 	{

src/core/VstSyncController.cpp
@@ -28,7 +28,6 @@
 #include <stdexcept>
 
 #include <QDebug>
-#include <QUuid>
 
 #include "AudioEngine.h"
 #include "ConfigManager.h"
@@ -44,7 +43,7 @@ VstSyncController::VstSyncController()
 {
 	try
 	{
-		m_syncData.create(QUuid::createUuid().toString().toStdString());
+		m_syncData.create();
 	}
 	catch (const std::runtime_error& error)
 	{